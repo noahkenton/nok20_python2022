e831ebd3e6763cae0b7b4120903b650ac49f6615
Noah Kenton <79272447+noahkenton@users.noreply.github.com>
2022-10-19 00:54:32 -0400

    major commit 2

--- assignment2/assignment2_database.sql
+++ assignment2/assignment2_database.sql
@@ -1,54 +1,57 @@
 Use nok20;
 
-DROP TABLE IF EXISTS paitent;
+
 DROP TABLE IF EXISTS visit_procedure;
 DROP TABLE IF EXISTS visit_diagnosis;
 DROP TABLE IF EXISTS visit;
+DROP TABLE IF EXISTS patient;
 DROP TABLE IF EXISTS procedure_;
 DROP TABLE IF EXISTS doctor;
 DROP TABLE IF EXISTS diagnosis;
 
 
-CREATE TABLE IF NOT EXISTS paitent (
-	paitent_uuid VARCHAR(50) NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS patient (
+		patient_uuid VARCHAR(100) PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    sysmptoms VARCHAR(30) NOT NULL
-    ) Engine = InnoDB;
-    
-CREATE TABLE IF NOT EXISTS visit (
-	visit_uuid VARCHAR(50) NOT NULL PRIMARY KEY,
-    visit_date DATETIME NOT NULL,
-    fk_doctor_uuid VARCHAR(50) NOT NULL,
-    fk_paitent_uuid VARCHAR(50) NOT NULL
-    ) Engine = InnoDB;
-    
-CREATE TABLE IF NOT EXISTS visit_procedure (
-	fk_visit_uuid VARCHAR(50) NOT NULL,
-    fk_procedure_uuid VARCHAR(50) NOT NULL,
-    FOREIGN KEY (fk_visit_uuid) REFERENCES visit (visit_uuid) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (fk_procedure_uuid) REFERENCES visit (visit_uuid) ON DELETE CASCADE ON UPDATE CASCADE    
-    ) Engine = InnoDB;
-
-CREATE TABLE IF NOT EXISTS procedure_ (
-	procedure_uuid VARCHAR(50) NOT NULL PRIMARY KEY,
-    procedure_name VARCHAR(20) NOT NULL
+    symptoms VARCHAR(30) NOT NULL
     ) Engine = InnoDB;
     
 CREATE TABLE IF NOT EXISTS doctor (
-	doctor_uuid VARCHAR(50) NOT NULL PRIMARY KEY,
+		doctor_uuid VARCHAR(100) PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL
     ) Engine = InnoDB;
     
+CREATE TABLE IF NOT EXISTS visit (
+			visit_uuid VARCHAR(100) PRIMARY KEY,
+		visit_date DATETIME NOT NULL,
+		fk_doctor_uuid VARCHAR(100),
+		fk_patient_uuid VARCHAR(100),
+    FOREIGN KEY (fk_doctor_uuid) REFERENCES doctor (doctor_uuid) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_patient_uuid) REFERENCES patient (patient_uuid) ON DELETE CASCADE ON UPDATE CASCADE 
+    ) Engine = InnoDB;
+ 
+CREATE TABLE IF NOT EXISTS procedure_ (
+		procedure_uuid VARCHAR(100) PRIMARY KEY,
+    procedure_name VARCHAR(30) NOT NULL
+    ) Engine = InnoDB;
+    
+CREATE TABLE IF NOT EXISTS visit_procedure (
+		fk_visit_uuid VARCHAR(50),
+		fk_procedure_uuid VARCHAR(50),
+    FOREIGN KEY (fk_visit_uuid) REFERENCES visit (visit_uuid) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_procedure_uuid) REFERENCES procedure_ (procedure_uuid) ON DELETE CASCADE ON UPDATE CASCADE    
+    ) Engine = InnoDB;
+    
 CREATE TABLE IF NOT EXISTS diagnosis (
-	diagnosis_uuid VARCHAR(50) NOT NULL PRIMARY KEY,
+	diagnosis_uuid VARCHAR(100) NOT NULL PRIMARY KEY,
     diagnosis VARCHAR(30) NOT NULL
     ) Engine = InnoDB;    
     
 CREATE TABLE IF NOT EXISTS visit_diagnosis (
-	fk_visit_uuid VARCHAR(50) NOT NULL,
-    fk_diagnosis_uuid VARCHAR(50) NOT NULL,
+	fk_visit_uuid VARCHAR(100),
+    fk_diagnosis_uuid VARCHAR(100),
     FOREIGN KEY (fk_visit_uuid) REFERENCES visit (visit_uuid) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (fk_diagnosis_uuid) REFERENCES diagnosis (diagnosis_uuid) ON DELETE CASCADE ON UPDATE CASCADE    
     ) Engine = InnoDB;
